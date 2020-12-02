5b311939964bbb6f0614e2b716f2fd880723d03f
Gnk Afridi <gnk@nextgeni.com>
2020-12-02 16:21:08 +0500

    socket test

---
+++ index.js
@@ -0,0 +1,72 @@
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+// app.use(express.json());
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+ console.log("Server running on port "+port);
+});
+  
+// http.listen(port, function(){
+//     console.log('listening on *:' + port);
+// });
+
+// app.post("/create-socket", (req, res, next) => {
+//     var roomname = req.body.roomname;
+//     var username =  req.body.username;
+    
+//     io.on('connection', function(socket){
+//         socket.on('create', function(roomname){
+//             socket.join(roomname);
+//         });
+//     });
+
+//     res.json(username+", has successfully connected on room:"+roomname);
+
+//     // res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+//     // console.log(req.body);
+// });
+
+
+  
+// io.on('connection', (socket) => {
+
+//     socket.on('create', function(room) {
+//         console.log('room-create: '+ room);
+//         socket.join(room);
+//     });
+
+//     socket.on('publish', function(room, data){
+//         console.log('data-publish: ', data);
+//         socket.to(room).emit('data: ', data);
+//     });
+
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected');
+//     });
+
+// });
+
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socket = require('socket.io');
+const io = socket(server);
+
+io.on('connection', onConnection);
+
+function onConnection(socket){
+socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
+
+const port = 8080;
+server.listen(port, () => console.log(`server is running on port ${port}`));
