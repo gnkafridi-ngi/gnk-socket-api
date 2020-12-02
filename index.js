21301b0334c1416b094fe5879c9fad839e6ceb37
Gnk Afridi <gnk@nextgeni.com>
2020-12-02 16:28:45 +0500

    re-deployed

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require("express");
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+// var express = require("express");
+// var app = express();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+// var port = process.env.PORT || 3000;
 
 // app.use(express.json());
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname + '/index.html');
+// });
 
-app.listen(port, () => {
- console.log("Server running on port "+port);
-});
+// app.listen(port, () => {
+//  console.log("Server running on port "+port);
+// });
   
 // http.listen(port, function(){
 //     console.log('listening on *:' + port);
